4d9553a8325b227ce1bd32cc6e90f45fc4cce003
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-11 01:42:29 -0400

    hijack logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,18 +1,21 @@
 import {$log} from "@tsed/logger";
+const setLoggers = () => {
+  $log.appenders.set("stdout", {
+    type: "file",
+    filename: "logs/stdout.log",
+    levels: ["info", "debug", "error", "warn", "trace", "fatal"],
+    layout: {
+      type: "json"
+    }
+  });
+  $log.appenders.set("stderr", {
+    levels: ["trace", "fatal", "error", "warn"],
+    type: "file",
+    filename: "logs/stderr.log",
+    layout: {
+      type: "json"
+    }
+  });
 
-$log.appenders.set("stdout", {
-  type: "file",
-  filename: "logs/stdout.log",
-  levels: ["info", "debug", "error", "warn", "trace", "fatal"],
-  layout: {
-    type: "json"
-  }
-});
-$log.appenders.set("stderr", {
-  levels: ["trace", "fatal", "error", "warn"],
-  type: "file",
-  filename: "logs/stderr.log",
-  layout: {
-    type: "json"
-  }
-});
+}
+export default setLoggers();
