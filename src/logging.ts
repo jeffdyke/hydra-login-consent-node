28b9dfbd85097122ed622f85bafd2630d2ef6b1f
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-11 00:53:07 -0400

    logging and callbacks

---
+++ src/logging.ts
@@ -0,0 +1,18 @@
+import {$log} from "@tsed/logger";
+
+$log.appenders.set("stdout", {
+  type: "file",
+  filename: "logs/stdout.log",
+  levels: ["info", "debug", "error", "warn", "trace", "fatal"],
+  layout: {
+    type: "json"
+  }
+});
+$log.appenders.set("stderr", {
+  levels: ["trace", "fatal", "error", "warn"],
+  type: "file",
+  filename: "logs/stderr.log",
+  layout: {
+    type: "json"
+  }
+});
