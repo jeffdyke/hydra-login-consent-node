4ac6f5ce5f09a3c3d2542839ef86773c7ec0eb3d
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-10 20:19:20 -0400

    update packages

--- src/google_auth.ts
+++ src/google_auth.ts
@@ -4,15 +4,13 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
-}, async (accessToken, refreshToken, profile, done) => {
-  const user = {
-    googleId: profile.id,
-    name: profile.displayName,
-    email: profile.emails?.[0]?.value,
-    avatar: profile.photos?.[0]?.value,
-  };
-  return done(null, user);
-}));
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
