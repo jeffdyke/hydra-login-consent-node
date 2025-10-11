a886db8b6feaccf75b530ceba951ae1d38afee49
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-11 14:56:16 -0400

    logging and types

--- src/google_auth.ts
+++ src/google_auth.ts
@@ -2,6 +2,8 @@ import { HYDRA_URL } from './config.js';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import qs from "querystring";
+import {CollectionOf, Minimum, Property, Description} from "@tsed/schema";
+import {Configuration} from "@tsed/di";
 import { auth, OAuth2Client, TokenPayload } from 'google-auth-library';
 import jsonLogger  from "./logging.js"
 dotenv.config()
@@ -15,6 +17,33 @@ export type UserInfo = {
   picture: string;
   locale: string;
 }
+@Configuration({
+  jsonMapper: {
+    additionalProperties: false,
+    disableUnsecureConstructor: false,
+    strictGroups: false
+  }
+})
+export class GoogleTokenResponse {
+  @Property()
+  access_token: string | undefined;
+
+  @Property()
+  expires_in: number | undefined;
+
+  @Property()
+  scope: string | undefined;
+
+  @Property()
+  token_type: string | undefined;
+
+  @Property()
+  id_token?: string; // Optional: Present if OpenID Connect scopes are requested
+
+  @Property()
+  refresh_token?: string; // Optional: Present if offline access is requested
+}
+
 export const authClientConfig = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -43,7 +72,7 @@ async function getGoogleUser(access_token: string, id_token: string): Promise<Us
     .catch((err) => { jsonLogger.info("Error is %s", err); return err.response.data })  ;
 
 }
-async function googleTokenResponse(code: string) {
+async function googleTokenResponse(code: string): Promise<GoogleTokenResponse> {
     const body = new URLSearchParams({
       code,
       client_id: authClientConfig.clientId || '',
