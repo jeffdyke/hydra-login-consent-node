84e940e50b9daa2f3007403cc7a4c55cb9186c1e
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-11 12:53:18 -0400

    log paths

--- src/logging.ts
+++ src/logging.ts
@@ -1,27 +1,24 @@
 import {Logger} from "@tsed/logger";
-import {StdoutAppender, StderrAppender} from "@tsed/logger-std";
-import {FileAppender} from "@tsed/logger-file";
-import {JsonLayout} from "@tsed/logger/layouts/JsonLayout.js";
 
 const APP_LOG = new Logger()
 
-  APP_LOG.appenders.set("std-log-json", {
-    type: "file",
-    filename: `${__dirname}/logs/app.log`,
-    pattern: ".yyyy-MM-dd",
-    levels: ["info", "debug", "error", "warn", "trace", "fatal"],
-    layout: {
-      type: "json"
-    }
-  })
+APP_LOG.appenders.set("std-log-json", {
+  type: "file",
+  filename: `/var/log/hydra/app.log`,
+  pattern: ".yyyy-MM-dd",
+  levels: ["info", "debug", "error", "warn", "trace", "fatal"],
+  layout: {
+    type: "json"
+  }
+})
 
-  APP_LOG.appenders.set("console-log", {
-    type: "console",
-    levels: ["info", "debug", "error", "warn", "trace", "fatal"],
-    layout: {
-      type: "json"
-    }
-  })
+APP_LOG.appenders.set("console-log", {
+  type: "console",
+  levels: ["info", "debug", "error", "warn", "trace", "fatal"],
+  layout: {
+    type: "json"
+  }
+})
 
 
 
