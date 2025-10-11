53ec7a10941a9509d8cf1ec82008b0637637bdfd
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-10 22:27:09 -0400

    async google functions

--- src/google_auth.ts
+++ src/google_auth.ts
@@ -13,16 +13,24 @@ export const authClientConfig = {
     };
 const GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
 
-const oauthToken = function(code: string) {
+export async function authPost(code: string) {
   let header = {
         "Content-Type": "application/x-www-form-urlencoded",
       }
 
-    axios.post(
+    await axios.post(
         GOOGLE_TOKEN_URL,
         qs.stringify({...authClientConfig, code: code}),
         { headers: header }
       ).then((resp) => { console.log("Response is %s", resp.data); return resp.data})
       .catch((err) => { console.log("Error is %s", err); return err.response.data });
+
+  }
+
+  export async function getUserInfo(access_token: string, id_token: string) {
+    const url = `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${access_token}`;
+    await axios.get(url, { headers: { Authorization: `Bearer ${id_token}` } })
+      .then((resp) => { return resp.data })
+      .catch((err) => { console.log("Error is %s", err); return err.response.data })  ;
 
   }
