7b26c932d6fa9a53eac46ddbd5d824948e41a32f
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-12 17:46:15 -0400

    copy views

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,9 @@ import typescript from '@rollup/plugin-typescript';
 import packageJson from './package.json' with { type: "json" };
 import dts from "rollup-plugin-dts";
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 import json from '@rollup/plugin-json';
 const config = [
-  // {
-  //   input: 'lib/app.js',
-  //   output: {
-  //     file: 'lib/bundle/hydra-consent-node.js',
-  //     format: 'cjs',
-  //     sourcemap: true,
-  //   },
-  //   external: packageJson.dependencies ? Object.keys(packageJson.dependencies) : [],
-  //   plugins: [typescript(), resolve(), json(), commonjs({
-  //         include: 'node_modules/**' // Ensure it processes modules in node_modules
-  //   })]
-  // // }, {
-  // //   input: 'build/compiled/index.d.ts',
-  // //   output: {
-  // //     file: 'hydra-consent-node.d.ts',
-  // //     format: 'es'
-  // //   },
-  // //   plugins: [dts()]
-
-  // },
   {
     input: 'src/app.ts',
     output: {
@@ -33,7 +13,14 @@ const config = [
       sourcemap: true,
     },
     external: packageJson.dependencies ? Object.keys(packageJson.dependencies) : [],
-    plugins: [typescript(), resolve(), json()]
+    plugins: [typescript(), resolve(), json(),
+    copy({
+        targets: [
+          { src: 'views/**/*', dest: 'lib/bundle/views' } // Copy the 'views' folder and its contents
+        ],
+        flatten: false // Preserve the directory structure within 'views'
+      })
+    ]
   }
 ];
 
