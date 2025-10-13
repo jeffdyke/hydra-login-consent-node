52f033ab9990f8485fc0aa57990732df73d508e0
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-13 17:17:48 -0400

    bring client-fetch local

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,19 @@
 // Copyright © 2025 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
-import { Configuration, ConfigurationParameters } from "@ory/hydra-client";
-import { OAuth2Api } from "@ory/client-fetch"
+import { Configuration, ConfigurationParameters } from "client-fetch-local";
+import { OAuth2Api } from "client-fetch-local"
+import queryString from "query-string"
 const baseOptions: any = {}
 baseOptions.basePath = process.env.HYDRA_ADMIN_URL || "http://localhost:4445"
 baseOptions.accessToken = process.env.ORY_API_KEY || process.env.ORY_PAT
-
+baseOptions.queryParamsStringify = queryString.stringify
 if (process.env.MOCK_TLS_TERMINATION) {
   baseOptions.headers = { "X-Forwarded-Proto": "https" }
 }
 
 const HYDRA_URL = "https://auth.staging.bondlink.org"
 
-const hydraAdmin = new OAuth2Api(baseOptions )
+const hydraAdmin = new OAuth2Api(baseOptions)
 
 // PostgreSQL configuration
 const pgConfig = {
