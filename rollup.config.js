e59bda33aad7ae7b58ebfc583749e50edbfd6191
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-13 00:03:02 -0400

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,22 @@ import dts from "rollup-plugin-dts";
 import resolve from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
 const config = [
   {
     input: 'src/app.ts',
     output: {
       dir: 'dist',
-      preserveModules: true,
       entryFileNames: '[name].js',
       format: 'esm',
       sourcemap: true,
     },
-    external: packageJson.dependencies ? Object.keys(packageJson.dependencies) : [],
-    plugins: [typescript(), resolve(), json(),
+    plugins: [typescript(), json(), resolve({jsnext: true}), commonjs({include: ['src/app.ts', 'node_modules/**']}),
     copy({
         targets: [
-          { src: 'views/**/*', dest: 'dist/views' } // Copy the 'views' folder and its contents
+          { src: 'views/**/*', dest: 'dist/views' }, // Copy the 'views' folder and its contents
+          { src: 'node_modules', dest: 'dist' }
+
         ],
         flatten: false // Preserve the directory structure within 'views'
       })
