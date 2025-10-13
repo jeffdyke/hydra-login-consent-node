c097c37fb85b40234a787a76ac7b60da1b8bba5f
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-12 21:19:02 -0400

    update log creation format

--- src/logging.ts
+++ src/logging.ts
@@ -1,18 +1,15 @@
-import {Logger} from "@tsed/logger";
-import {JsonLayout} from "@tsed/logger/layouts/JsonLayout.js";
-const APP_LOG = new Logger()
+import {$log} from "@tsed/logger";
 
-APP_LOG.appenders.set("std-log-json", {
+$log.appenders.set("std-log-json", {
   type: "file",
   filename: `/var/log/hydra/app.log`,
   pattern: ".yyyy-MM-dd",
   levels: ["info", "debug", "error", "warn", "trace", "fatal"],
   layout: {
-    type: JsonLayout
+    type: "json"
   }
 })
-
-APP_LOG.appenders.set("console-log", {
+  .set("console-log", {
   type: "console",
   levels: ["info", "debug", "error", "warn", "trace", "fatal"],
   layout: {
@@ -20,6 +17,4 @@ APP_LOG.appenders.set("console-log", {
   }
 })
 
-
-
-export default APP_LOG
+export default $log
