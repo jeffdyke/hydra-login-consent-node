da8c4bedbaecd50961fd7667ead7d00fab1c862e
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-14 09:56:04 -0400

    don't allow multiple clients

--- build/create_oauth2_client.sh
+++ build/create_oauth2_client.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-set -xe
+set -e
 # This script creates an OAuth2 client in Hydra and generates a .env file for the consent app.
 HOST_IP=$(ipconfig getifaddr en0)
 ISSUER="http://${HOST_IP}:4444"
 ISSUER_ADMIN="http://${HOST_IP}:4445"
 CALLBACK_HOST="http://${HOST_IP}:3000"
-
+if [ -f .env ]; then
+  echo ".env file already exists. Please delete it if you want to recreate the OAuth2 client."
+  exit 1
+fi
+echo "Creating OAuth2 client in Hydra at $ISSUER_ADMIN"
 client_output=$(hydra create client \
   --endpoint "${ISSUER_ADMIN}" \
   --name "ory-hydra-test-application" \
@@ -16,18 +20,10 @@ client_output=$(hydra create client \
   --format json
   )
 
-# hydra create client
-#   --endpoint http://$(ipconfig getifaddr en0):4445
-#   --token-endpoint-auth-method none
-#   --scope "offline email openid offline_access"
-#   --grant-type "client_credentials, authorization_code, refresh_token"
-#   --redirect-uri "http://$(ipconfig getifaddr en0):3000/callback"
-# hydra get oauth2-client 2ace26a5-8e89-4cb3-8360-095c4a26e8b1 --endpoint http://0.0.0.0:4445 --format json | jq '.'
 client_id=$(echo "$client_output" | jq -r '.client_id')
 client_secret=$(echo "$client_output" | jq -r '.client_secret')
 
 echo "Client ID: $client_id"
-echo "Client Secret: $client_secret"
 echo "Creating .env file"
 
 cat <<EOF > .env
