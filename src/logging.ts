aa40a90e94d0e889a3ac99f47f3641b3f5582025
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-14 21:01:46 -0400

    update logging

--- src/logging.ts
+++ src/logging.ts
@@ -2,21 +2,19 @@ import {$log} from "@tsed/logger";
 
 $log.appenders.set("std-log-json", {
   type: "file",
-  filename: "/var/log/hydra/app.log",
+  filename: `logs/app.log`,
   pattern: ".yyyy-MM-dd",
   levels: ["info", "debug", "error", "warn", "trace", "fatal"],
   layout: {
     type: "json"
   }
 })
-
-$log.appenders.set("console-log", {
+  .set("console-log", {
   type: "console",
   levels: ["info", "debug", "error", "warn", "trace", "fatal"],
   layout: {
     type: "json"
   }
-
 })
 
 export default $log
