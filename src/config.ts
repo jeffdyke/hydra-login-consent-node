03f1480f2242ab58445c248e06f773623b1883db
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-15 20:58:29 -0400

    argh, just save

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,12 @@
 // Copyright © 2025 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
-import { Configuration, ConfigurationParameters } from "@ory/hydra-client-fetch";
+import { Configuration } from "@ory/hydra-client-fetch";
 import { OAuth2Api } from "@ory/hydra-client-fetch/dist/index.js"
-import { json } from "body-parser";
-import queryString from "query-string";
+import session from "express-session"
+import connectPgSimple from "connect-pg-simple"
+import pool from "./pool.js"
 import jsonLogger from "./logging.js"
 const baseOptions: any = {}
-// baseOptions.basePath = process.env.HYDRA_ADMIN_URL || "http://dev.bondlink.org:4445"
-// baseOptions.accessToken = process.env.ORY_API_KEY || process.env.ORY_PAT
-// baseOptions.queryParamsStringify = queryString.stringify
 if (process.env.MOCK_TLS_TERMINATION) {
   baseOptions.headers = { "X-Forwarded-Proto": "https" }
 }
@@ -18,6 +16,9 @@ const configuration = new Configuration({
   headers: baseOptions.headers,
 })
 const CLIENT_ID = process.env.AUTH_FLOW_CLIENT_ID || ""
+if (!CLIENT_ID) {
+  throw new Error("CLIENT_ID environment is not legit `{process.env.AUTH_FLOW_CLIENT_ID}`")
+}
 const hydraAdmin = new OAuth2Api(configuration)
 // jsonLogger.info("Hydra Admin URL: %s", hydraAdmin.middleware)
 // PostgreSQL configuration
@@ -30,4 +31,16 @@ const pgConfig = {
   port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
 }
 
+const PgStore = connectPgSimple(session)
+const hasClientId = () => {
+  let s = "select * from hydra_client where id = $1"
+  const res = pool.query(s, [CLIENT_ID]);
+  return res
+}
+let exists = hasClientId()
+if (!exists) {
+  throw new Error("clientId returned false, this is required, query failed")
+} else {
+  jsonLogger.info("ClientId %s", JSON.stringify(exists))
+}
 export { hydraAdmin, pgConfig, CLIENT_ID}
