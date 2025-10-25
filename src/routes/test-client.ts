869b7899a279467fac85108d6072d338a4547572
Jeff Dyke <jeff.dyke@gmail.com>
2025-10-25 16:31:09 -0400

    add test-client

---
+++ src/routes/test-client.ts
@@ -0,0 +1,27 @@
+import {newClient,getClient} from "../authFlow.js"
+import { hydraAdmin } from "../setup/hydra.js"
+import express from "express"
+import {v4} from "uuid"
+import jsonLogger from "../logging.js"
+import { OAuth2Client } from "google-auth-library"
+
+const router = express.Router()
+router.get("/", (req, res) => {
+  const cId = v4()
+  jsonLogger.info("Creating client", {clientId:cId})
+
+  try {
+    newClient(cId).then(client => {
+      jsonLogger.info("Created client", {client: client, from:cId})
+      getClient(cId).then(getC => {
+        jsonLogger.info("new client", {c:getC})
+      })
+    })
+  } catch(err) {
+    jsonLogger.info("Error creating client", {error: err})
+  }
+  const mst = "Done with client creation ${clientId}"
+  res.status(200).send(mst)
+})
+
+export default router
