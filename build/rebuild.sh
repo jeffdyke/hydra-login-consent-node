47200d5aec74f060823923ad499e5358ef2d8ff5
Jeff Dyke <jeff.dyke@gmail.com>
2025-11-04 13:29:38 -0500

    renames

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -2,5 +2,5 @@
 sudo bash build.sh clean && \
   aws ecr get-login-password | sudo docker login --username AWS --password-stdin https://668874212870.dkr.ecr.us-east-1.amazonaws.com && \
   sudo docker system prune -a -f && \
-  sudo bash build.sh build hydra-consent-node && \
-  sudo bash build.sh push hydra-consent-node
+  sudo bash build.sh build hydra-headless-ts && \
+  sudo bash build.sh push hydra-headless-ts
