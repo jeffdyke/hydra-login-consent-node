943891284bfcc7bf2215051e5e99e24c2ec1a000
Jeff Dyke <jeff.dyke@gmail.com>
2025-11-09 20:57:24 -0500

    cahnge to 200

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,21 +6,21 @@ const router = express.Router();
 router.head('/', (req, res) => {
   jsonLogger.info('HEAD / request received', { headers: req.headers });
   res.set('X-BondLink-Special', 'Head-Only-Value');
-  res.status(204).end();
+  res.status(200).end();
 });
 
 
 router.get('/', (req, res) => {
   jsonLogger.info('GET / request received', { headers: req.headers });
   res.set('X-BondLink-Special', 'Head-Only-Value');
-  res.status(204).end();
+  res.status(200).end();
 });
 
 
 router.post('/', (req, res) => {
   jsonLogger.info('POST / request received', { headers: req.headers });
   res.set('X-BondLink-Special', 'Head-Only-Value');
-  res.status(204).end();
+  res.status(200).end();
 });
 
 
