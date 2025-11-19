75bf53248856e039a43256a8f22ba6c76c1ed0a1
Jeff Dyke <jeff.dyke@gmail.com>
2025-11-19 13:15:32 -0500

    log locals

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -4,6 +4,7 @@ import logger  from "../logging.js";
 import type { Request, Response, NextFunction } from "express";
 
 export function requestLogger(req: Request, res: Response, next: NextFunction) {
+
   const start = process.hrtime.bigint();
   const logStarts = /Claude-User|python-http/
   const stripArgs = /login.?login_challenge|consent.?consent_challenge|consent/
@@ -25,7 +26,7 @@ export function requestLogger(req: Request, res: Response, next: NextFunction) {
     if (req.originalUrl !== "/favicon.ico"){
       const end = process.hrtime.bigint();
       const duration = Number(end - start) / 1_000_000;
-
+      res.locals.logData.body = res.locals
       logger.info(`Request finished: ${req.method} ${req.originalUrl.split("?")[0]}`, {
         statusCode: res.statusCode,
         durationMs: duration.toFixed(2),
