d96160c2035cf116a539414c348a0e643cf75073
Jeff Dyke <jeff.dyke@gmail.com>
2025-11-30 17:41:03 -0500

    docker

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -4,6 +4,6 @@ if [ "$1" = "force" ]; then
   sudo docker stop hydra-headless-ts-1
   sudo docker system prune -a -f
 fi
-docker build -f Dockerfile.headless-ts -t jeffdyke/hydra-headless-ts:latest .
+docker build -f build/Dockerfile.headless-ts -t jeffdyke/hydra-headless-ts:latest .
 docker push jeffdyke/hydra-headless-ts:latest
 sudo docker compose up -d --force-recreate headless-ts
